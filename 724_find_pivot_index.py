87e80ea4f2e188a973dee81c774a6a8d200150a2
Mark Cruz <cruzm430@gmail.com>
2023-01-31 17:03:53 -0500

    first commit

---
+++ 724_find_pivot_index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum = 0 
+        right_sum = sum(nums)
+        for idx, num in enumerate(nums):
+            right_sum -= num
+            if left_sum == right_sum:
+                return idx
+            else:
+                left_sum += num
+        else:
+            return -1
